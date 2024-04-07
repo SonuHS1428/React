6cf1eaebb496b835a675bd593b631f47054476b0
SonuHS1428 <Sonu HS>
2024-04-07 17:10:32 +0530

    initial commit

---
+++ color_picker/src/App.js
@@ -0,0 +1,11 @@
+import ColorPicker from "./ColorPicker";
+import './index.css';
+function App() {
+  return (
+    <div>
+      <ColorPicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ color_picker/src/ColorPicker.js
@@ -0,0 +1,24 @@
+//ColorPicker: An exercise for using useState() hook and onChange() event handler
+
+import React,{useState} from 'react';
+function ColorPicker(){
+    
+    const [color,setColor]=useState("#00FFFF");
+
+    function handleColorChange(event){
+        setColor(event.target.value);
+    }
+    
+    return(
+        <div className='color-picker-container'>
+            <h1>Color Picker</h1>
+            <div className='color-display' style={{backgroundColor:color}}>
+                <p>Selected Color:{color}</p>
+            </div>
+            <label>Select a color    </label>
+            <input type='color' value={color} onChange={handleColorChange}/>
+        </div>
+    );
+}
+
+export default ColorPicker
